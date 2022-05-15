091f16542ac3f9003bb8f0c4b0e3f1473eb74170
bingyue <99388013+bingqiu456@users.noreply.github.com>
2022-05-15 09:19:39 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+ # -*- coding: utf-8 -*-
+import setuptools
+setuptools.setup(
+    name = "nonebot_plugin_yulu",
+    version = "2.0",
+    packages = setuptools.find_packages(),
+    author="bingyue",
+    author_email="hello-yiqiu@qq.com",
+    description="""语录娱乐小插件""",
+    url="https://github.com/bingqiu456/nonebot_plugin_yulu",
+    install_requires=[
+        "bs4>=0.0.1",
+        "beautifulsoup4>=4.11.1",
+        "nonebot2>=2.0.0b2",
+        "nonebot-adapter-onebot>=2.0.0b1"
+    ],
+    keywords=["nonebot_plugin_yulu","nonebot","nonebot_plugin"],
+    package_data={
+        'nonebot_plugin_yulu':['jitang.txt','yiyan.txt'],
+    }
+)
