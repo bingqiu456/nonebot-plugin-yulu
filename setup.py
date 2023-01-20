6aab1f719a2cba43881ed376aa200ef1a760650e
bingyue <99388013+bingqiu456@users.noreply.github.com>
2023-01-20 18:18:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 setuptools.setup(
     name = "nonebot_plugin_yulu",
-    version = "2.0",
+    version = "3.0",
     packages = setuptools.find_packages(),
     author="bingyue",
     author_email="hello-yiqiu@qq.com",
